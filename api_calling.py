865327e58cb93cbc5c7f9149c69fcbfd7e80ee11
pranamitaaa <pranamitadeb74@gmail.com>
2026-04-17 14:36:20 +0600

    Update import statement and configure genai client

--- api_calling.py
+++ api_calling.py
@@ -1,4 +1,4 @@
-from google import genai
+import google.generativeai as genai
 import os,io
 from dotenv import load_dotenv
 
@@ -9,7 +9,7 @@ my_api_key = os.getenv("GEMINI_API_KEY")
 
 #initializing a client 
 client = genai.Client(api_key= my_api_key)
-
+genai.configure(api_key=my_api_key)
 def error_explanation(images,selected_option):
     prompt=f"Explain the errors occurs in the {images}."
     response=client.models.generate_content(
